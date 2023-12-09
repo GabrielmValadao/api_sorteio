e4ba65f3e55a0bc30266e4c3b31e248a539e8872
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-09 12:05:15 -0300

    feat: desenvolve a sintaxe da seeder de awards

--- api_sorteio/database/seeders/PopulateAwardsTable.php
+++ api_sorteio/database/seeders/PopulateAwardsTable.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Award;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class PopulateAwardsTable extends Seeder
      */
     public function run(): void
     {
-        //
+        Award::create([
+            'name' => '',
+            'descption' => 'local',
+            'value' => '',
+            'amount' => '',
+            'date' => '',
+        ]);
     }
 }
