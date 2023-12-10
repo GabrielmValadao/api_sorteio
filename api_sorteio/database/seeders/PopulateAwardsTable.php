c53230945fd7c628474761685aff1e4f8ebf72d8
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-10 12:43:33 -0300

    feat: instala a biblioteca faker e utiliza ela no projeto

--- api_sorteio/database/seeders/PopulateAwardsTable.php
+++ api_sorteio/database/seeders/PopulateAwardsTable.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Award;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class PopulateAwardsTable extends Seeder
 {
@@ -13,12 +14,18 @@ class PopulateAwardsTable extends Seeder
      */
     public function run(): void
     {
-        Award::create([
-            'name' => '',
-            'descrption' => 'local',
-            'value' => '',
-            'amount' => '',
-            'date' => '',
-        ]);
+
+        $faker = Factory::create();
+
+        for($i = 0; $i < 10; $i++) {
+
+            Award::create([
+                'descrption' => $faker->text(),
+                'local' => $faker->text(),
+                'value' => $faker->randomFloat(2, 100, 1000),
+                'amount' => $faker->numberBetween(1, 5),
+                'date' => $faker->date,
+            ]);
+        }
     }
 }
