8bda14d99f96a38dd3539287ace5117e5894de0f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-12-10 13:32:58 -0300

    feat: cria o envio do email para o usuario sorteado

---
+++ api_sorteio/app/Mail/SendAwardsToClient.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendAwardsToClient extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+
+    public $client;
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Parabéns, você foi sorteado!',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            html: 'view.SendAward',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
